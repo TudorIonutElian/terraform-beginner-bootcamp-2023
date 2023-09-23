0a7b25d1b303c26b00f939f11ede7787500cc9d0
Ionut - Elian TUDOR <ionuteliantudor@gmail.com>
2023-09-23 16:50:12 +0000

    Add terraform provider, configure it and add s3 bucket resource

--- main.tf
+++ main.tf
@@ -4,7 +4,14 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
   }
+}
+
+provider "aws" {
 }
 
 provider "random" {
@@ -16,6 +23,13 @@ resource "random_string" "bucket_name" {
   special  = false
   lower = true
   upper = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "terraform_course_bucket" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
