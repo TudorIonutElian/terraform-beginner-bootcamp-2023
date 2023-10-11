33c046a46bbe11b80d94b6ca6ac08c1cf909a0ab
Ionut - Elian TUDOR <ionuteliantudor@gmail.com>
2023-10-11 18:11:33 +0000

    Continue with terraform custom provider 2.2.0 #47

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    required_providers {
+      terratowns = {
+        source = "local.providers/local/terratowns"
+        version = "1.0.0"
+      }
+    }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "ExamPro"
@@ -16,12 +22,18 @@ terraform {
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="9aa5ce1e-b551-4b62-aa89-70294efa85ca" 
+  token="9aa5ce1e-b551-4b62-aa89-70294efa85ca"
 }
+
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#  assets_path = var.assets_path
+#}
