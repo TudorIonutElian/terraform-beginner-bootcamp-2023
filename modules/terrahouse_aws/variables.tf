5a9a761e2a8e280f050d2f88ef9dbb950cf5e216
Ionut - Elian TUDOR <ionuteliantudor@gmail.com>
2023-10-13 16:47:38 +0000

    Terratowns Test 2.5.0 #53 comment bucket name

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -9,18 +9,18 @@ variable "user_uuid" {
 }
 
 # Declare variable for bucket name and validation rules
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-
-  validation {
-    condition     = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
-      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
-    )
-    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
-  }
-}
+#variable "bucket_name" {
+#  description = "The name of the S3 bucket"
+#  type        = string
+#
+#  validation {
+#    condition     = (
+#      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+#      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+#    )
+#    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+#  }
+#}
 
 # Declare index_html_filepath receiving variable
 variable "index_html_filepath" {
